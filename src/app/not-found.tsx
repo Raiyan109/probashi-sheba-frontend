5ba79e610ac1c3e72b4b3b8b2fb045acc16c18b9
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2025-04-15 17:38:22 +0600

    fixed hydration warning and checked if shadcn working by adding button in not found route

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div>
-      <h2 className='text-green-600'>Not Found</h2>
+      <h2>Not Found</h2>
       <p>Could not find requested resource</p>
-      <Link href="/"> Return Home </Link>
+      <Button><Link href="/"> Return Home </Link></Button>
     </div>
   )
 }
