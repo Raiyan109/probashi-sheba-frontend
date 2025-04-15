c94695dccc8456e326dc42da500641f876a6e887
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2025-04-15 17:29:01 +0600

    added shadcn ui

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,12 @@
+import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
 export default function NotFound() {
   return (
     <div>
-      <h2>Not Found</h2>
+      <h2 className='text-green-600'>Not Found</h2>
       <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <Link href="/"> Return Home </Link>
     </div>
   )
 }
