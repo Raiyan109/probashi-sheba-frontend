325d7c246a754dcbf46fd88b23b0e96dded465d0
Raiyan109 <kraiyan109@gmail.com>
2025-04-20 12:39:12 +0600

    key impact and checklist pages created

--- src/app/(adminPanelLayout)/admin/checklist/page.tsx
+++ src/app/(adminPanelLayout)/admin/checklist/page.tsx
@@ -1,8 +1,44 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import Link from "next/link"
 
 
 const ChecklistPage = () => {
     return (
-        <div>ChecklistPage</div>
+        <div>
+            <div className="flex items-center justify-between mb-5">
+                <h1>Checklist</h1>
+                <Link href={'/admin/checklist/add-checklist'} className="addButton">Add Checklist</Link>
+            </div>
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Title (English)</TableHead>
+                        <TableHead>Title (Bangla)</TableHead>
+                        <TableHead>Description (English)</TableHead>
+                        <TableHead>Description (Bangla)</TableHead>
+                        {/* <TableHead className="text-right">Category</TableHead> */}
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="font-medium">INV001</TableCell>
+                        <TableCell>Paid</TableCell>
+                        <TableCell>Credit Card</TableCell>
+                        <TableCell className="text-right">$250.00</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+
+        </div>
     )
 }
 
--- src/app/(adminPanelLayout)/admin/keyImpacts/page.tsx
+++ src/app/(adminPanelLayout)/admin/keyImpacts/page.tsx
@@ -1,8 +1,42 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import Link from "next/link"
 
 
 const KeyImpactsPage = () => {
     return (
-        <div>KeyImpactsPage</div>
+        <div>
+            <div className="flex items-center justify-between mb-5">
+                <h1>Key Impacts</h1>
+                <Link href={'/admin/keyImpacts/add-keyImpacts'} className="addButton">Add Key Impact</Link>
+            </div>
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Image</TableHead>
+                        <TableHead className="w-[100px]">Title (English)</TableHead>
+                        <TableHead>Title (Bangla)</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="font-medium">INV001</TableCell>
+                        <TableCell>Paid</TableCell>
+                        <TableCell>Credit Card</TableCell>
+                        <TableCell className="text-right">$250.00</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+
+        </div>
     )
 }
 
