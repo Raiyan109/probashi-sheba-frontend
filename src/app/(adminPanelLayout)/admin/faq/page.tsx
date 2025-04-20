80d0b4d4e0d740c91e3f270c30f6741428d5215f
Raiyan109 <kraiyan109@gmail.com>
2025-04-20 12:31:31 +0600

    faq done

--- src/app/(adminPanelLayout)/admin/faq/page.tsx
+++ src/app/(adminPanelLayout)/admin/faq/page.tsx
@@ -21,10 +21,11 @@ const FaqPage = () => {
                 <TableCaption>A list of your recent invoices.</TableCaption>
                 <TableHeader>
                     <TableRow>
-                        <TableHead className="w-[100px]">Invoice</TableHead>
-                        <TableHead>Status</TableHead>
-                        <TableHead>Method</TableHead>
-                        <TableHead className="text-right">Amount</TableHead>
+                        <TableHead className="w-[100px]">Qustion (English)</TableHead>
+                        <TableHead>Qustion (Bangla)</TableHead>
+                        <TableHead>Answer (English)</TableHead>
+                        <TableHead>Answer (Bangla)</TableHead>
+                        <TableHead className="text-right">Category</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
